37e776baa5a46a34a9a673fcb2fd4db117cde17c
Prince <princeuzgang@gmail.com>
2022-03-04 01:36:48 +0500

    admin panel tayyor redux, react icons ornatdim

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { burgerCounter } from "../../redux/actions";
+
+import { BsJustify } from "react-icons/bs";
+
+import "./navbar.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counterReducer.counter);
+
+  const handleClick = () => {
+    dispatch(burgerCounter(1));
+  };
+  return (
+    <div className="navbarTop d-flex justify-content-around align-items-center">
+      <div>
+        <button className="navButton" onClick={() => handleClick()}>
+          <BsJustify className="navBurgerIcon" />
+        </button>
+      </div>
+      <div>
+        <h1>Codemy Admin Panel</h1>
+      </div>{" "}
+    </div>
+  );
 };
 
 export default Navbar;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./root";
+import { Provider } from "react-redux";
+import store from './redux/store.js'
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
